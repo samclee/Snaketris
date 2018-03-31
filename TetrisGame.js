af86d8401a6e027e80a7d55041f71e22a3d50e92
dyefcee <samclee@gmail.com>
2018-03-31 09:41:10 -0700

    Blocks now fall in randomized sequence, stick at bottom
    When a block reaches the bottom, it remains there and a new piece is
    produced.
    
    TODO:
    Find and clear full rows
    Game over conditions

--- TetrisGame.js
+++ TetrisGame.js
@@ -16,10 +16,11 @@ function TetrisGame(gW, gH, tS)
 
   this.pieceType = 0;
   this.pieceRot = 0;
-
   this.pieceX = 3;
   this.pieceY = 0;
 
+  this.newSeq();
+  this.newPiece();
 }
 
 TetrisGame.prototype.reset = function()
@@ -31,15 +32,38 @@ TetrisGame.prototype.reset = function()
       this.inert[y][x] = ' ';
     }
   }
+
+  this.newSeq();
+  this.newPiece();
 } // reset()
 
 TetrisGame.prototype.newPiece = function()
 {
   this.pieceX = 3;
   this.pieceY = 0;
-  this.pieceType = 0;
   this.pieceRot = 0;
-} // newPiece
+
+  this.pieceType = this.sequence.pop();
+  if(this.sequence.length === 0)
+  {
+    this.newSeq();
+  }
+} // newPiece()
+
+TetrisGame.prototype.newSeq = function()
+{
+  let seq = [0, 1, 2, 3, 4, 5, 6]; // n=7
+
+  for(var i=0; i<5; i++)
+  {
+    let j = Math.floor(Math.random() * (7-i)) + i;
+    let t = seq[j];
+    seq[j] = seq[i];
+    seq[i] = t;
+  } // bottom-up Fisher-Yates
+  console.log(seq);
+  this.sequence = seq;
+} // newSeq()
 
 TetrisGame.prototype.update = function()
 {
@@ -52,6 +76,18 @@ TetrisGame.prototype.update = function()
   } // piece can fall
   else
   {
+    for(var y=0; y<4; y++)
+    {
+      for(var x=0; x<4; x++)
+      {
+        let block = pieceStructures[this.pieceType][this.pieceRot][y][x];
+        if(block !== ' ')
+        {
+          this.inert[this.pieceY + y][this.pieceX + x] = block; 
+        } // stamp block into inert grid
+      }
+    }
+
     this.newPiece();
   }
 
@@ -60,16 +96,15 @@ TetrisGame.prototype.update = function()
 
 TetrisGame.prototype.canPieceMove = function(testX, testY, testRot)
 {
+  let orientation = pieceStructures[this.pieceType][testRot];
   for(var y=0; y<4; y++)
   {
     for(var x=0; x<4; x++)
-    {
-      let isNotEmpty = pieceStructures[this.pieceType][this.pieceRot][y][x] !== ' ';
-      
+    {      
       let testXLoc = testX + x;
       let testYLoc = testY + y;
 
-      if(isNotEmpty && (  (testXLoc < 0) || 
+      if((orientation[y][x] !== ' ') && (  (testXLoc < 0) || 
                           (testXLoc >= this.gridWdt) || 
                           (testYLoc >= this.gridHgt) || 
                           (this.inert[testYLoc][testXLoc] !== ' ')  ))
